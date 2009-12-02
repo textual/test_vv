ff545c8251246d03608acd635c9ec33a26563a1a
Federico Gonzalez <federico@innku.com>
2009-12-02 12:49:13 -0600

    Fixed: Error in button_to_add_album

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,7 @@ module ArtistsHelper
   
   def button_to_add_an_album(form)
     button_to_function("Add an album") do |page|
-      form.fields_for(:album, Album.new, :child_index => '_INDEX_') do |album_form|
+      form.fields_for(:albums, Album.new, :child_index => '_INDEX_') do |album_form|
         html = render :partial => 'album_form', :locals => {:album_form => album_form}
         page << %{Element.insert("albums", { bottom: update_index_in(#{html.to_json})})}
       end
