aab02e7746f8786e217a5819dfaf5f24da55498b
Federico Gonzalez <federico@innku.com>
2009-12-01 19:06:07 -0600

    Fixed: seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,20 @@ mozart = Artist.create(:name => "Wolfgang Amadeus Mozart")
 Artist.create(:name => "Elvis Presley")
 Artist.create(:name => "Aretha Franklin")
 
-Album.create!(:title => "Every Time We Touch", :year => 2006, :condition => "like new", :artist => cascada)
-Album.create!(:title => "Perfect Day", :year => 2007, :condition => "possessed", :artist => cascada)
-Album.create!(:title => "Evacuate the Dancefloor", :year => 2009, :artist => cascada)
+Album.create!(:title => "Every Time We Touch", :year => 2006, :condition => "like new", :artist_id => cascada.id)
+Album.create!(:title => "Perfect Day", :year => 2007, :condition => "possessed", :artist_id => cascada.id)
+Album.create!(:title => "Evacuate the Dancefloor", :year => 2009, :artist_id => cascada.id)
 
-Album.create!(:title => "Sonata number 12", :year => 1745, :artist => mozart, :condition => "unknown")
-Album.create!(:title => "Sonata number 13", :year => 1746, :artist => mozart, :condition => "boring")
-Album.create!(:title => "Funky Time party", :year => 1749, :artist => mozart, :condition => "used")
+Album.create!(:title => "Sonata number 12", :year => 1745, :artist_id => mozart.id, :condition => "unknown")
+Album.create!(:title => "Sonata number 13", :year => 1746, :artist_id => mozart.id, :condition => "boring")
+Album.create!(:title => "Funky Time party", :year => 1749, :artist_id => mozart.id, :condition => "used")
 
 
 andrew = Artist.create!(:name => "Andrew")
-Album.create!(:title => "Andrew's rock opera", :year => 1973, :condition => "like new", :artist => andrew)
-Album.create!(:title => "Andrew's jazz fusion", :year => 1973, :condition => "dirty", :artist => andrew)
-Album.create!(:title => "Andrew's metal band", :year => 1985, :condition => "scratched", :artist => andrew)
+Album.create!(:title => "Andrew's rock opera", :year => 1973, :condition => "like new", :artist_id => andrew.id)
+Album.create!(:title => "Andrew's jazz fusion", :year => 1973, :condition => "dirty", :artist_id => andrew.id)
+Album.create!(:title => "Andrew's metal band", :year => 1985, :condition => "scratched", :artist_id => andrew.id)
 
 andrew = Artist.create!(:name => "Tim")
-Album.create!(:title => "Tim's punk rock album", :year => 1982, :condition => "like gnu", :artist => andrew)
+Album.create!(:title => "Tim's punk rock album", :year => 1982, :condition => "like gnu", :artist_id => andrew.id)
 
