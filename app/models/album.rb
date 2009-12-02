4e7b74a5fe3ebfaa6554bb2ca783e1d712cd2804
unknown <dan@.(none)>
2009-12-02 13:26:11 -0800

    added default url for images

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,9 +2,10 @@ class Album < ActiveRecord::Base
   
   belongs_to  :artist
   has_many :tracks
-  has_attached_file :image #, :styles => {:medium => "300x300>", :thumb => "100x100>"}
+  has_attached_file :image, :default_url => "/images/music_clipart/1.jpg"  #, :styles => {:medium => "300x300>", :thumb => "100x100>"}
   
   ALBUM_CONDITIONS = ["mint", "good", "average", "poor"]
+
   
   validates_presence_of :title, :year, :condition
   validates_inclusion_of :condition, :in => ALBUM_CONDITIONS, :message => "{{value}} is not valid", :allow_blank => true
